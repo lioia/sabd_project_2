0ba2d2e8b5c7980af504ca6c2a569fabfe8e87fa
Alessandro Lioi <lioialessandro2000@live.com>
2024-06-24 15:04:45 +0200

    feat(scripts): start containers and create Kafka topics

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+echo "Cleaning up"
+docker compose stop
+docker compose rm -f
+
+echo "Starting containers"
+docker compose up -d
+
+echo "Creating Kafka topics"
+docker exec broker sh -c \
+  "/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic original"
+docker exec broker sh -c \
+  "/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic filtered"
+docker exec broker sh -c \
+  "/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic invalid"
