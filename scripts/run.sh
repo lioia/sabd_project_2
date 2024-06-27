7d3ac3aec8240a44eba31d407fc3556ed17d0d16
Alessandro Lioi <lioialessandro2000@live.com>
2024-06-27 18:39:36 +0200

    feat(scripts): run now reflects what the main program expects

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+# $1: query number (1, 2 or empty for both)
+
+# Start Dataset Replay
 docker exec jobmanager sh -c \
-  "/opt/flink/bin/flink run --python /app/main.py"
+  "curl http://producer:8888/replay"
+
+# Run Query 1
+if [[ -z "$1" || "$1" == "1" ]]; then
+  docker exec jobmanager sh -c \
+    "/opt/flink/bin/flink run --python /app/main.py 1"
+fi
+
+# Run Query 2
+if [[ -z "$1" || "$1" == "2" ]]; then
+  docker exec jobmanager sh -c \
+    "/opt/flink/bin/flink run --python /app/main.py 2"
+fi
